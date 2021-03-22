e09f263db780146f1d931203e89d7ab9197d369a
gboliknow <gboliknow@gmail.com>
2021-03-22 06:40:22 -0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/kakulato/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kakulato
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/CalcButton.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CalcButton extends StatelessWidget {
+  final String text;
+  final int fillColor;
+  final int textColor;
+  final double textSize;
+  final Function callback;
+  final int focusColor;
+
+  const CalcButton({
+    Key key,
+    this.text,
+    this.fillColor,
+    this.textColor = 0xFFFFFFFF,
+    this.textSize = 28,
+    this.callback,
+    this.focusColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: SizedBox(
+        width: 65,
+        height: 65,
+        child: FlatButton(
+           hoverColor: Colors.yellow,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50.0),
+            
+          ),
+          onPressed: () {
+            callback(text);
+            
+          },
+          child: Text(
+            text,
+            style: GoogleFonts.rubik(
+              textStyle: TextStyle(
+                fontSize: textSize,
+              ),
+            ),
+          ),
+          color: fillColor != null ? Color(fillColor) : Colors.black54,
+          textColor: Color(textColor),
+        ),
+      ),
+    );
+  }
+}
